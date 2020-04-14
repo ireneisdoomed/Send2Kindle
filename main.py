32617353557e7f3c9d61bde09aa22e8ff56bc4fe
ireneisdoomed <irene.lopezs@protonmail.com>
2020-04-14 13:03:08 +0200

    Attachment created.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import smtplib
 from login import from_email, pssw
 import email
 from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 from email import encoders
 
@@ -15,22 +16,39 @@ except Exception as e:
     print(e)
 
 
-# Adjunto
-
-file = "GWAS.mobi"
+# Creo objeto
 
 mail = MIMEMultipart()
 mail["From"] = from_email
 mail["To"] = from_email
 mail["Subject"] = "Convert"
 
-#De mi objeto Multipart voy añadiéndole cada una de las partes
-body = MIMEText("text", "plain")
-content = "This message is from python"
-body.set_payload("")
-mail.attach(body)
+# De mi objeto Multipart voy añadiéndole cada una de las partes
+part1 = MIMEText("text", "plain")
+content = "This message is from Python."
+part1.set_payload(content)
+mail.attach(part1)
+
+# Adjunto archivo
 
 # Mando el email
+
+file = "GWAS.mobi"
+
+with open(file, "rb") as attachment:
+  # Creo otro objeto MIME con el formato predeterminado para mandar un archivo binario
+  part2 = MIMEBase("application", "octet-stream")
+  part2.set_payload(attachment.read())
+
+# Codifico el archivo en caraceres ASCII
+
+encoders.encode_base64(part2)
+
+
+# Añado el objeto a mi objeto multiparte como antes
+
+mail.attach(part2)
+
 
 server.sendmail(
   from_email,
