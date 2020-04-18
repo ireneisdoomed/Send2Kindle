ae75ea329a4130e552547efbf17efac46689168a
ireneisdoomed <irene.lopezs@protonmail.com>
2020-04-18 20:10:08 +0200

    app running. No conversion

--- send2kindle.py
+++ send2kindle.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
 import argparse
+from src.main import *
 
-def main():
+def parsear():
     # Inicializo parser
     parser = argparse.ArgumentParser(description="A tool to automate the process of uploading an ebook to your Kindle.")
 
@@ -11,7 +12,18 @@ def main():
     parser.add_argument("to", type=str, help="Your Kindle e-mail address.")
 
     args = parser.parse_args() # Parsea los argumentos y los devuelve de una manera en la que podamos trabajar
+
     return args
+
+def main():
+    args = parsear()
+    server = crearConexion()
+    
+    mail = crearMIME(args.file, args.to)
+
+    mandar(args.to, mail, server)
+    cerrarConexion(server)
+    server = 0
 
 
 if __name__ == "__main__":
---
+++ src/main.py
@@ -0,0 +1,73 @@
+import smtplib
+from src.login import from_email, pssw
+import email
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email import encoders
+
+def crearConexion():
+  # Conexión segura con el servidor
+
+  server = smtplib.SMTP_SSL(host='smtp.gmail.com', port=465)
+  try:
+      server.login(from_email, pssw)
+      print("Successful connection!")
+      return server
+  except Exception as e:
+      print(e)
+
+
+def crearMIME(file, to_email):
+  # Creo objeto
+
+  mail = MIMEMultipart()
+  mail["From"] = from_email
+  mail["To"] = to_email
+  mail["Subject"] = "Convert"
+
+  # De mi objeto Multipart voy añadiéndole cada una de las partes
+  part1 = MIMEText("text", "html")
+  content = "You can follow me <a href='www.elpais.es'>here</a> for more projects."
+  part1.set_payload(content)
+  mail.attach(part1)
+
+  # Adjunto archivo
+
+  with open(file, "rb") as attachment:
+    # Creo otro objeto MIME con el formato predeterminado para mandar un archivo binario
+    part2 = MIMEBase("application", "octet-stream")
+    part2.set_payload(attachment.read())
+
+  # Codifico el archivo en caracteres ASCII
+
+  encoders.encode_base64(part2)
+
+  # Añado nombre al archivo
+
+  part2.add_header(
+      "Content-Disposition",
+      "attachment; filename= {}".format(file),
+  )
+
+  # Añado el objeto a mi objeto multiparte como antes
+
+  mail.attach(part2)
+
+  return mail
+
+
+def mandar(to_email, mail, server):
+  # Envío e-mail
+  try:
+    server.sendmail(
+      from_email,
+      to_email,
+      mail.as_string()) # Sólo admite string
+  except Exception:
+    print("El envío ha fallado.")
+
+def cerrarConexion(server):
+  # Cierro la conexión con el servidor
+  server.quit()
+  print("Done! Your ebook is in your e-mail awaiting authorization.")
